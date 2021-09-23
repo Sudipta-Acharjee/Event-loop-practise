47c8ed4dae16741a5bd9e23dd11999d251fdb80a
unknown <sudiptoacharjee4@gmail.com>
2021-09-23 09:20:01 +0600

    updated

--- Event-loop.js
+++ Event-loop.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const fs = require("fs");
+
+setTimeout(() => console.log('Timer 1 finished'), 0);
+setImmediate(() => console.log('Timer 1 finished'));
+
+fs.readFile("text-file.txt", () => {
+    console.log("I/0 finished");
+})
+
+console.log("Hello from the top level code");
