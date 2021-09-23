c05a0ab886aae83aad5367fc3151fc1d517f3d9d
unknown <sudiptoacharjee4@gmail.com>
2021-09-23 12:39:26 +0600

    updated

--- Event-loop.js
+++ Event-loop.js
@@ -5,6 +5,12 @@ setImmediate(() => console.log('ImmadiateTimer 1 finished'));
 
 fs.readFile("test-file.txt", () => {
     console.log("I/0 finished");
+    console.log("----------------------------------------------------------------")
+    setTimeout(() => console.log('Timer 1 finished'), 0);
+    setImmediate(() => console.log('ImmadiateTimer 1 finished'), 3000);
+
+
+    process.nextTick(() => console.log('process.nexttick'))
 })
 
 console.log("Hello from the top level code");
