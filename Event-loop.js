abc8eb6a222eca5d994146d98b1c6295907d1512
unknown <sudiptoacharjee4@gmail.com>
2021-09-23 12:42:02 +0600

    updated

--- Event-loop.js
+++ Event-loop.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('ImmadiateTimer 1 finished'));
@@ -11,6 +12,7 @@ fs.readFile("test-file.txt", () => {
 
 
     process.nextTick(() => console.log('process.nexttick'))
+    crypto.pbkdf2('password', 'salt');
 })
 
 console.log("Hello from the top level code");
