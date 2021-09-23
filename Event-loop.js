b8b900fd08654656a73a08642bf72870cda0eac3
unknown <sudiptoacharjee4@gmail.com>
2021-09-23 10:20:32 +0600

    update

--- Event-loop.js
+++ Event-loop.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
-setImmediate(() => console.log('Timer 1 finished'));
+setImmediate(() => console.log('ImmadiateTimer 1 finished'));
 
-fs.readFile("text-file.txt", () => {
+fs.readFile("test-file.txt", () => {
     console.log("I/0 finished");
 })
 
