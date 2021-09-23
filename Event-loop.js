e64711685cd45fa665b58fbf916be1c72262c1e3
unknown <sudiptoacharjee4@gmail.com>
2021-09-23 12:48:01 +0600

    Time-taken

--- Event-loop.js
+++ Event-loop.js
@@ -16,6 +16,15 @@ fs.readFile("test-file.txt", () => {
     crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
         console.log(Date.now() - start, "Password encrypted");
     });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
 })
 
 console.log("Hello from the top level code");
