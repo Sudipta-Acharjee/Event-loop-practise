95651cb40d491869df0ab4ec014b9784a308a8cb
unknown <sudiptoacharjee4@gmail.com>
2021-09-24 08:16:59 +0600

    connect with server

--- events.js
+++ events.js
@@ -1,14 +1,12 @@
 const EventEmitter = require("events");
-
+const http = require("http");
 class Sales extends EventEmitter {
     constructor() {
         super();
     }
 }
 
-
 const myEmitter = new Sales();
-
 
 myEmitter.on("newSale", () => {
     console.log("There was a new sale!")
@@ -22,3 +20,20 @@ myEmitter.on("newSale", stock => {
     console.log(`There are now ${stock} items`)
 })
 myEmitter.emit("newSale", 100);
+
+/////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log('Request Recieved');
+    res.end('Request Recieved');
+});
+
+server.on('close', () => {
+    console.log('Server Closed');
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log('waiting for request');
+});
