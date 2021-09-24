56643945d62430f03b5148b4d5eac1689b5952f5
unknown <sudiptoacharjee4@gmail.com>
2021-09-24 08:42:19 +0600

    bug fixed

--- streams.js
+++ streams.js
@@ -1,2 +1,14 @@
-const fs = require('fs');
-const server = require('http').createServer;
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+    //Solution 1
+    fs.readFile("test-file.txt", (err, data) => {
+        if (err) console.error(err);
+        res.end(data);
+    })
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening...")
+});
