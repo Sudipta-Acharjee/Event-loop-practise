8c1bfc4e3fb1a68de016123ae81f3624730adecb
unknown <sudiptoacharjee4@gmail.com>
2021-09-28 09:55:18 +0600

    another module created

--- modules.js
+++ modules.js
@@ -1,2 +1,11 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+
+//module.exports 
+const C = require('./test-module-1');
+
+const cal1 = new C();
+console.log(cal1.add(2, 5));
+
+//exports;
---
+++ test-module-1.js
@@ -0,0 +1,12 @@
+module.exports = class {
+    add(a, b) {
+        return a + b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+}
+
